8eee0f58f375170b38cb8ecdf8b42fa7768b603c
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-04-22 12:35:24 +0000

    update

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-import express from 'express'
-import  {ApolloServer}  from 'apollo-server-express'
-import cors from 'cors'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
 //Import
 import { db } from "./Config/db.js";
 import { resolver } from "./GraphQl/Resolver/index.js";
 import { schema } from "./GraphQl/Schema/index.js";
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
-async function startApolloServer(schema, resolver,) {
-    const server = new ApolloServer({
-        schema,
-        rootValue: resolver,
-        graphiql: true,
-        context: ({ req }) => ({ req })
-    }); 
-    const app = express();
-    app.use(cors());
-    await server.start();
-    server.applyMiddleware({app, path: '/graphql'});
-    db.then(() => {
+async function startApolloServer(schema, resolver) {
+  const server = new ApolloServer({
+    schema,
+    rootValue: resolver,
+    graphiql: true,
+    context: ({ req }) => ({ req })
+  });
+  const app = express();
+  app.use(cors());
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+  db.then(() => {
     app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
-    })
-    }).catch((err) => {
-        console.error(err);
+      console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
     });
+  }).catch((err) => {
+    console.error(err);
+  });
 }
 
 startApolloServer(schema, resolver);
- 
