2841948b590318f7888579029c06a5a4a612c167
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2024-08-17 12:39:29 +0200

    update

--- GraphQl/Resolver/like.js
+++ GraphQl/Resolver/like.js
@@ -29,5 +29,33 @@ export default {
         } else {
             throw new Error('Authentication  failled')
         }
-     }
+     },
+     disLike: async ({
+      postID,
+       },
+       context
+      ) =>{
+          const { username } = CheckAuth(context)
+          if( username){
+           try {
+              const post = await Post.findById(postID);
+              if(post) {
+                 post.likes = post.likes.filter(like => like.username !== username);
+                 const postSaved =  await post.save();
+                 return {
+                    id: postSaved._id,
+                    ...postSaved._doc
+                 }
+              }else {
+               throw new Error("Post not found");
+              }
+           } catch(error) {
+              return Promise.resolve(new GraphQLError(error.message));
+           }
+          } else {
+              throw new Error('Authentication  failled')
+          }
+       }
+     
+
 }
