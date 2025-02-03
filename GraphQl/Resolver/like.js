09414bba92c02435d60a59f22338ad76d87b8532
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2025-02-03 15:29:26 +0100

    fix:feature like

--- GraphQl/Resolver/like.js
+++ GraphQl/Resolver/like.js
@@ -1,61 +1,44 @@
-import { GraphQLError } from "graphql"
+import { GraphQLError } from "graphql";
 import { Post } from "../../Model/index.js";
 import CheckAuth from "../../Auth/Check_Auth.js";
 
-
 export default {
-     like: async ({
-        postID,
-     },
-     context
-    ) =>{
-        const { username } = CheckAuth(context)
-        if( username){
-         try {
-            const post = await Post.findById(postID);
-            if(post) {
-               post.likes.unshift({
-                  username
-               });
-               const postSaved =  await post.save();
-               return {
-                  id: postSaved._id,
-                  ...postSaved._doc
-               }
-            }
-         } catch(error) {
-            return Promise.resolve(new GraphQLError(error.message));
-         }
-        } else {
-            throw new Error('Authentication  failled')
-        }
-     },
-     disLike: async ({
-      postID,
-       },
-       context
-      ) =>{
-          const { username } = CheckAuth(context)
-          if( username){
-           try {
-              const post = await Post.findById(postID);
-              if(post) {
-                 post.likes = post.likes.filter(like => like.username !== username);
-                 const postSaved =  await post.save();
-                 return {
-                    id: postSaved._id,
-                    ...postSaved._doc
-                 }
-              }else {
-               throw new Error("Post not found");
-              }
-           } catch(error) {
-              return Promise.resolve(new GraphQLError(error.message));
-           }
+  like: async ({ postID }, context) => {
+    const { username } = CheckAuth(context);
+    if (username) {
+      try {
+        const post = await Post.findById(postID);
+        if (post) {
+          if (post.likes.find((like) => like.username === username)) {
+            // Post already like
+            post.likes = post.likes.filter(
+              (like) => like.username !== username
+            );
+            const postSaved = await post.save();
+            return {
+              id: postSaved._id,
+              ...postSaved._doc
+            };
           } else {
-              throw new Error('Authentication  failled')
+            // No like
+            post.likes.push({
+              username,
+              createdAt: new Date().toISOString()
+            });
+            const postSaved = await post.save();
+            return {
+              id: postSaved._id,
+              ...postSaved._doc
+            };
           }
-       }
-     
-
-}
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (error) {
+        return Promise.resolve(new GraphQLError(error.message));
+      }
+    } else {
+      throw new Error("Authentication  failled");
+    }
+  }
+};
--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-import express from 'express'
-import  {ApolloServer}  from 'apollo-server-express'
-import { PubSub } from 'graphql-subscriptions';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
 //Import
 import { db } from "./Config/db.js";
 import { resolver } from "./GraphQl/Resolver/index.js";
 import { schema } from "./GraphQl/Schema/index.js";
+import { pubsub } from "./GraphQl/Utils/pubsub.js";
 
-const PORT = 5000
+const PORT = 5000;
 
-async function startApolloServer(schema, resolver,) {
-    const pubsub = new PubSub();
-    
-    const server = new ApolloServer({
-        schema,
-        rootValue: resolver,
-        graphiql: true,
-        context: async ({ req }) => {
-            return { req, pubsub }
-        }  
-    
-    }); 
-    const app = express();
-    await server.start();
-    server.applyMiddleware({app, path: '/graphql'});
-    db.then(() => {
+async function startApolloServer(schema, resolver) {
+  const server = new ApolloServer({
+    schema,
+    rootValue: resolver,
+    graphiql: true,
+    context: async ({ req }) => {
+      return { req, pubsub };
+    }
+  });
+  const app = express();
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+  db.then(() => {
     app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
-    })
-    }).catch((err) => {
-        console.error(err);
+      console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
     });
+  }).catch((err) => {
+    console.error(err);
+  });
 }
 
 startApolloServer(schema, resolver);
- 
